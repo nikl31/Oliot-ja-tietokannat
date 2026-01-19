ba2d42dad8263e33f4df74ba84b81dbe4d41b4ea
nikl31 <niklas.kotilainen@student.lab.fi>
2026-01-19 20:51:09 +0000

    main.py

--- Week 1/W1_T2/main.py
+++ Week 1/W1_T2/main.py
@@ -1,1 +1,30 @@
+from car import Car
 
+
+def main():
+    print("Program starting.")
+    print("Initializing three car objects.")
+
+    car1 = Car("red")
+    car2 = Car("green")
+    car3 = Car("blue")
+
+    print("Car objects initialized.")
+
+    print("Starting the first car object.")
+    car1.start()
+
+    print("Starting the second car object.")
+    car2.start()
+
+    print("Starting the third car object.")
+    car3.start()
+
+    print("Starting the third car object.")
+    car3.start()
+
+    print("Program ending.")
+
+
+if __name__ == "__main__":
+    main()
