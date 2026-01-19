d35c572a9108ac7e4e7a88ee9a0ff3361e863e12
nikl31 <niklas.kotilainen@student.lab.fi>
2026-01-19 20:51:02 +0000

    car.py

--- Week 1/W1_T2/car.py
+++ Week 1/W1_T2/car.py
@@ -1,1 +1,12 @@
+class Car:
+    def __init__(self, color: str):
+        self.color = color
+        self.engine_on = False
 
+    def start(self) -> None:
+        if not self.engine_on:
+            print(f"{self.color} car started.")
+            self.engine_on = True
+        else:
+            print(f"{self.color} is already running.")
+        return None
