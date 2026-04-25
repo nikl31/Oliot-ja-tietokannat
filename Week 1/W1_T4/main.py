0c19f48bfdb91d4450d2f5d83117f4e869b27f7e
nikl31 <niklas.kotilainen@student.lab.fi>
2026-04-25 13:19:40 +0000

    main.py

--- Week 1/W1_T4/main.py
+++ Week 1/W1_T4/main.py
@@ -19,12 +19,10 @@ class Main:
 
             elif choice == "2":
                 print(f"Currently '{ca.getAmount()}' coins in coin acceptor")
-                print()
 
             elif choice == "3":
                 returned = ca.returnCoins()
                 print(f"Coin acceptor returned '{returned}' coins.")
-                print()
 
             elif choice == "0":
                 print("Program ending.")
