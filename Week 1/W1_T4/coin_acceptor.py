dc82918f6605ad2e58f548d8c6546507b1a6f451
nikl31 <niklas.kotilainen@student.lab.fi>
2026-04-25 10:10:20 +0000

    coin_acceptor.py

--- Week 1/W1_T4/coin_acceptor.py
+++ Week 1/W1_T4/coin_acceptor.py
@@ -1,1 +1,14 @@
+class CoinAcceptor:
+    def __init__(self):
+        self.__amount = 0
 
+    def insertCoin(self) -> None:
+        self.__amount += 1
+
+    def getAmount(self) -> int:
+        return self.__amount
+
+    def returnCoins(self) -> int:
+        returned = self.__amount
+        self.__amount = 0
+        return returned
