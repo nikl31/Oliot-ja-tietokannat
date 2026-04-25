77fc9e92fa58a8e8eaff8788a1da1777e07975eb
nikl31 <niklas.kotilainen@student.lab.fi>
2026-04-25 18:26:20 +0000

    main.js

---
+++ Week 3/W3_T2/main.js
@@ -0,0 +1,26 @@
+const Person = require('./person');
+
+class Main {
+    constructor() {
+        console.log("Program starting.");
+        console.log("Creating person...");
+
+        const person = new Person("John", "Doe", 30);
+
+        console.log("Person created.");
+
+        console.log("Name: " + person.getFullname());
+        console.log("Age: " + person.getAge());
+
+        console.log("Person has now birthday...");
+
+        person.ageUp();
+        let age = person.getAge();
+
+        console.log("New age: " + age);
+
+        console.log("Program ending.");
+    }
+}
+
+new Main();
