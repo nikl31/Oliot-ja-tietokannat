83073a32a557302e543e2b3ff17c5c107b8f7bdb
nikl31 <niklas.kotilainen@student.lab.fi>
2026-04-25 10:02:13 +0000

    counter.py

--- Week 1/W1_T3/counter.py
+++ Week 1/W1_T3/counter.py
@@ -1,1 +1,13 @@
+class Counter:
+    def __init__(self):
+        self.__count = 0
 
+    def add_count(self) -> None:
+        self.__count += 1
+
+    @property
+    def count(self) -> int:
+        return self.__count
+
+    def reset(self) -> None:
+        self.__count = 0
