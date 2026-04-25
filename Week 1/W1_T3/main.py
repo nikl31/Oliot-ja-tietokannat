d454fb51817448dffb446ca4351424e1e2a237a0
nikl31 <niklas.kotilainen@student.lab.fi>
2026-04-25 09:17:51 +0000

    main.py

--- Week 1/W1_T3/main.py
+++ Week 1/W1_T3/main.py
@@ -1,1 +1,39 @@
+from counter import Counter
 
+def main():
+    print("Program starting.")
+    print("Initializing counter...")
+
+    counter = Counter()
+
+    print("Counter initialized.")
+
+    while True:
+        print("\nOptions:")
+        print("1) Add count")
+        print("2) Get count")
+        print("3) Zero count")
+        print("0) Exit program")
+
+        choice = input("Choice: ")
+
+        if choice == "1":
+            counter.addCount()
+            print("Count increased")
+
+        elif choice == "2":
+            print(f"Current count '{counter.getCount()}'")
+
+        elif choice == "3":
+            counter.zeroCount()
+            print("Count zeroed")
+
+        elif choice == "0":
+            print("\nProgram ending.")
+            break
+
+        else:
+            print("Invalid choice, try again.")
+
+if __name__ == "__main__":
+    main()
