ac995511303f1ec9e911b046123a7a0fdc566791
nikl31 <niklas.kotilainen@student.lab.fi>
2026-04-25 16:04:14 +0000

    soda_bottle.py

--- Week 2/W2_T1/soda_bottle.py
+++ Week 2/W2_T1/soda_bottle.py
@@ -1,1 +1,8 @@
 
+class SodaBottle:
+    def __init__(self, brand: str, volume: float):
+        self.brand = brand
+        self.volume = volume
+
+    def serialize(self) -> str:
+        return f"{self.brand};{self.volume}"
