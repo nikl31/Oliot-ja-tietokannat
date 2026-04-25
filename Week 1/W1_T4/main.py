567d264765f3dd518c2d2625eb41cd79729cb0d6
nikl31 <niklas.kotilainen@student.lab.fi>
2026-04-25 11:53:09 +0000

    main.py

--- Week 1/W1_T4/main.py
+++ Week 1/W1_T4/main.py
@@ -1,33 +1,33 @@
 from coin_acceptor import CoinAcceptor
 
+class Main:
+    def __init__(self) -> None:
+        print("Program starting.")
 
-def oo_main_structure():
-    print("Program starting.")
+        ca = CoinAcceptor()
 
-    ca = CoinAcceptor()
+        while True:
+            print("1 - Insert coin")
+            print("2 - Show coins")
+            print("3 - Return coins")
+            print("0 - Exit program")
 
-    while True:
-        print("1 - Insert coin")
-        print("2 - Show coins")
-        print("3 - Return coins")
-        print("0 - Exit program")
+            choice = input("Your choice: ")
 
-        choice = input("Your choice: ")
+            if choice == "1":
+                ca.insertCoin()
 
-        if choice == "1":
-            ca.insertCoin()
+            elif choice == "2":
+                print(f"Currently '{ca.getAmount()}' coins in coin acceptor")
 
-        elif choice == "2":
-            print(f"Currently '{ca.getAmount()}' coins in coin acceptor")
+            elif choice == "3":
+                returned = ca.returnCoins()
+                print(f"Coin acceptor returned '{returned}' coins.")
 
-        elif choice == "3":
-            returned = ca.returnCoins()
-            print(f"Coin acceptor returned '{returned}' coins.")
-
-        elif choice == "0":
-            print("Program ending.")
-            break
+            elif choice == "0":
+                print("Program ending.")
+                break
 
 
 if __name__ == "__main__":
-    oo_main_structure()
+    Main()
