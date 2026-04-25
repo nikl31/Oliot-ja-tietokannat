59512b56fee3f61181375df3d775d9b9c0ee4a7e
nikl31 <niklas.kotilainen@student.lab.fi>
2026-04-25 15:29:50 +0000

    main.py

--- Week 2/W2_T1/main.py
+++ Week 2/W2_T1/main.py
@@ -1,24 +1,25 @@
 from soda_bottle import SodaBottle
 
 
-def main():
-    print("Program starting.")
-    print("Constructing soda bottle.")
-
-    brand = input("Insert brand: ")
-    volume = float(input("Insert volume: "))
-
-    bottle = SodaBottle(brand, volume)
-
-    print("SodaBottle object created!")
-    print("Serializing SodaBottle object.")
-
-    serialized = bottle.serialize()
-
-    print("Serialized sodabottle:")
-    print(serialized)
-
-    print("Program ending.")
+class Main:
+    def __init__(self) -> None:
+        print("Program starting.")
+        print("Constructing soda bottle.")
+        
+        brand = input("Insert brand: ")
+        volume = input("Insert volume: ")
+        
+        bottle = SodaBottle(brand, volume)
+        
+        print("SodaBottle object created!")
+        print("Serializing SodaBottle object.")
+        
+        serialized = bottle.serialize()
+        
+        print("Serialized sodabottle:")
+        print(serialized)
+        
+        print("Program ending.")
 
 
 if __name__ == "__main__":
