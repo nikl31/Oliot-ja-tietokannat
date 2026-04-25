9e1e937d7b1f36edbcbc0f2f8cbae81b6a19e6d6
nikl31 <niklas.kotilainen@student.lab.fi>
2026-04-25 15:05:25 +0000

    main.py

--- Week 1/W1_T2/main.py
+++ Week 1/W1_T2/main.py
@@ -2,29 +2,30 @@ from car import Car
 
 
 class Main:
-    print("Program starting.")
-    print("Initializing three car objects.")
+    def __init__(self) -> None:
+        print("Program starting.")
+        print("Initializing three car objects.")
 
-    car1 = Car("red")
-    car2 = Car("green")
-    car3 = Car("blue")
+        car1 = Car("red")
+        car2 = Car("green")
+        car3 = Car("blue")
 
-    print("Car objects initialized.")
+        print("Car objects initialized.")
 
-    print("Starting the first car object.")
-    car1.start()
+        print("Starting the first car object.")
+        car1.start()
 
-    print("Starting the second car object.")
-    car2.start()
+        print("Starting the second car object.")
+        car2.start()
 
-    print("Starting the third car object.")
-    car3.start()
+        print("Starting the third car object.")
+        car3.start()
 
-    print("Starting the third car object.")
-    car3.start()
+        print("Starting the third car object.")
+        car3.start()
 
-    print("Program ending.")
+        print("Program ending.")
 
 
 if __name__ == "__main__":
-    app = Main()
+    Main()
