326f6ea06756e2e15665b996ec00b59a30d3569b
nikl31 <niklas.kotilainen@student.lab.fi>
2026-04-25 18:14:38 +0000

    menu.py

---
+++ Week 2/W2_T6/menu.py
@@ -0,0 +1,64 @@
+from soda_bottle import SodaBottle
+
+class Menu:
+    def __init__(self):
+        self.bottles = []
+
+    def show(self):
+        print("Menu:")
+        print("1 - Add bottle")
+        print("2 - Show bottles")
+        print("3 - Save bottle")
+        print("0 - Exit")
+
+    def handle_choice(self, choice: str):
+
+        if choice == "1":
+            print("Creating soda bottle.")
+            brand = input("Insert brand: ")
+            volume = float(input("Insert volume: "))
+            self.bottles.append(SodaBottle(brand, volume))
+
+        elif choice == "2":
+            print("#### Soda bottles ####")
+            for i, bottle in enumerate(self.bottles, start=1):
+                print(f"Bottle {i}:")
+                print(f"  brand - {bottle.brand}")
+                print(f"  volume - {bottle.volume}")
+            print("#### Soda bottles ####")
+
+        elif choice == "3":
+            print("Saving soda bottles...")
+
+            rows = []
+            for bottle in self.bottles:
+                rows.append(bottle.serialize() + "\n")
+
+            file = open("inventory.txt", "w")
+            file.write("".join(rows))
+            file.close()
+
+            print("Saving completed!")
+
+        elif choice == "0":
+            print("\nExiting program.")
+            return False
+
+        else:
+            print("Unknown option, try again.")
+
+        return True
+
+    def load_inventory(self):
+        print("Loading inventory...")
+
+        file = open("inventory.txt", "r")
+        lines = file.readlines()
+        file.close()
+
+        for line in lines:
+            if line.strip() != "":
+                bottle = SodaBottle.deserialize(line)
+                self.bottles.append(bottle)
+
+        print("Inventory loaded.")
