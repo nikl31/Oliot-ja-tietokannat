c403c3b279b7f0081acaef989c2a16ff2bee5db1
nikl31 <niklas.kotilainen@student.lab.fi>
2026-04-25 10:23:56 +0000

    main.py

--- Week 1/W1_T3/main.py
+++ Week 1/W1_T3/main.py
@@ -1,39 +1,40 @@
-from counter import Counter
+class Main:
+    def init(self) -> None:
+        print("Program starting.")
+        print("Initializing counter...")
 
-def main():
-    print("Program starting.")
-    print("Initializing counter...")
+        counter = Counter()
 
-    counter = Counter()
+        print("Counter initialized.")
 
-    print("Counter initialized.")
+        while True:
+            print("\nOptions:")
+            print("1) Add count")
+            print("2) Get count")
+            print("3) Zero count")
+            print("0) Exit program")
 
-    while True:
-        print("\nOptions:")
-        print("1) Add count")
-        print("2) Get count")
-        print("3) Zero count")
-        print("0) Exit program")
+            choice = input("Choice: ")
 
-        choice = input("Choice: ")
+            if choice == "1":
+                counter.addCount()
+                print("Count increased")
 
-        if choice == "1":
-            counter.addCount()
-            print("Count increased")
+            elif choice == "2":
+                print(f"Current count '{counter.getCount()}'")
 
-        elif choice == "2":
-            print(f"Current count '{counter.getCount()}'")
+            elif choice == "3":
+                counter.zeroCount()
+                print("Count zeroed")
 
-        elif choice == "3":
-            counter.zeroCount()
-            print("Count zeroed")
+            elif choice == "0":
+                print("\nProgram ending.")
+                break
 
-        elif choice == "0":
-            print("\nProgram ending.")
-            break
+            else:
+                print("Invalid option.")
 
-        else:
-            print("Invalid choice, try again.")
+                return None
 
-if __name__ == "__main__":
-    main()
+if name == "main":
+    app = Main()
