664430fa0c1dbb7fd4dbb6b46fbd07f3c2a32ec8
nikl31 <niklas.kotilainen@student.lab.fi>
2026-04-25 18:41:20 +0000

    main.dart

---
+++ Week 3/W3_T6/main.dart
@@ -0,0 +1,22 @@
+import 'person.dart';
+
+void main() {
+  print("Program starting.");
+  print("Creating person...");
+
+  var person = Person("John", "Doe", 30);
+
+  print("Person created.");
+
+  print("Name: " + person.getFullname());
+  print("Age: " + person.getAge().toString());
+
+  print("Person has now birthday...");
+
+  person.ageUp();
+  var age = person.getAge();
+
+  print("New age: " + age.toString());
+
+  print("Program ending.");
+}
