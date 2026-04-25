03fc4fac3bda859a8a92fd5cdc9b5b735055afe9
nikl31 <niklas.kotilainen@student.lab.fi>
2026-04-25 14:06:28 +0000

    main.py

--- Week 1/W1_T5/main.py
+++ Week 1/W1_T5/main.py
@@ -1,1 +1,36 @@
+from coin_acceptor import CoinAcceptor
 
+
+class Main:
+    def __init__(self) -> None:
+        print("Program starting.")
+        print("Welcome to coin acceptor program.")
+        print("Insert new coin by typing it's value (0 returns the money, -1 exits the program)")
+
+        ca = CoinAcceptor()
+
+        while True:
+            choice = input("\nInsert coin(0 return, -1 exit): ")
+
+            if choice == "-1":
+                print("Exiting program.")
+                break
+
+            value = float(choice)
+
+            if value == 0:
+                print("Returning coins...")
+                coins, total = ca.returnCoins()
+                print(f"{coins} coins with {total}€ value returned.")
+                print(f"Inserted coins - {ca.getAmount()}, value - {ca.getValue()}€")
+
+            else:
+                print("Inserting...")
+                ca.insertCoin(value)
+                print(f"Inserted coins - {ca.getAmount()}, value - {ca.getValue()}€")
+
+        print("\nProgram ending.")
+
+
+if __name__ == "__main__":
+    app = Main()
