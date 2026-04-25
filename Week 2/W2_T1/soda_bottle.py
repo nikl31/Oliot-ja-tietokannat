42dc10c8cbd504639c847088c74f92cad105492c
nikl31 <niklas.kotilainen@student.lab.fi>
2026-04-25 18:49:48 +0300

    Delete Week 2/W2_T1 directory

--- Week 2/W2_T1/soda_bottle.py
+++
@@ -1,7 +0,0 @@
-class SodaBottle:
-    def __init__(self, brand: str, volume: float):
-        self.brand = brand
-        self.volume = volume
-
-    def serialize(self) -> str:
-        return f"{self.brand};{self.volume}"
