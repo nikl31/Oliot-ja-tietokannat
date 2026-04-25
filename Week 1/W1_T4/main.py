56292485fc6236de0741b0cb71692681e86b96e4
nikl31 <niklas.kotilainen@student.lab.fi>
2026-04-25 10:12:06 +0000

    main.py

--- Week 1/W1_T4/main.py
+++ Week 1/W1_T4/main.py
@@ -1,18 +1,16 @@
 from coin_acceptor import CoinAcceptor
 
-def print_menu():
-    print("\n1 - Insert coin")
-    print("2 - Show coins")
-    print("3 - Return coins")
-    print("0 - Exit program")
-
 def main():
     print("Program starting.")
 
     coin_acceptor = CoinAcceptor()
 
     while True:
-        print_menu()
+        print("\n1 - Insert coin")
+        print("2 - Show coins")
+        print("3 - Return coins")
+        print("0 - Exit program")
+
         choice = input("Your choice: ")
 
         if choice == "1":
@@ -28,6 +26,7 @@ def main():
         elif choice == "0":
             print("Program ending.")
             break
+
 
 if __name__ == "__main__":
     main()
